423f3d04726aedea702d16bf1b160b386a952fcf
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-16 02:53:20 +0000

    addition and substraction done

--- app.rb
+++ app.rb
@@ -2,15 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  redirect("/add")
+  erb(:add_form)
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
@@ -23,6 +24,8 @@ end
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
+
+  @result = @second_num - @first_num
 
   erb(:sub_result)
 end
